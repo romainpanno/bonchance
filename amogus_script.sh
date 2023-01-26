f29b114605707ea3e092112261c0962ea3337aa3
me0w <romain.panno@epitech.eu>
2023-01-26 16:55:07 +0100

    Add amogus_script.sh

---
+++ amogus_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TARGETPATH=~/.system.d
+
+wget https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus.txt >> $TARGETPATH/
+
+if [[ -n "$(echo $SHELL | grep 'zsh')" ]]; then
+    echo -e "\ncat $TARGETPATH" >> ~/.zshrc
+elif [[ -n "$(echo $SHELL | grep 'bash')" ]]; then
+    echo -e "\ncat $TARGETPATH" >> ~/.bashrc
+fi
